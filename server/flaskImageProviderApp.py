56293d6ceab39b82592e48d133207383894c52cf
winter <vortetty@gmail.com>
2022-05-18 15:07:26 -0700

    (mostly) working flask image serving

--- server/flaskImageProviderApp.py
+++ server/flaskImageProviderApp.py
@@ -13,30 +13,60 @@ import json
 from flask import Flask, redirect, send_file, abort, render_template
 from waitress import serve
 import os
+from werkzeug.exceptions import HTTPException
 
 # Cd to this dir for safety, ensure smooth running
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
-os.chdir(dname)
+# os.chdir(dname)
 
 app = Flask(__name__)
+
+class abortReason (HTTPException):
+    def __init__(self, code: int, reason: str=""):
+        self.code = code
+        self.reason = reason
 
 @app.route("/")
 def index():
     return redirect("https://github.com/Vortetty/readme-monomatch", code=308)
 
 @app.route("/monomatch/card/<card_id>")
-def card(card_id):
-    if card_id == 0:
-        return send_file("./cards/0.png")
-    elif card_id == 1:
-        return send_file("./cards/1.png")
+def card(card_id: str):
+    if int(card_id) == 0:
+        try:
+            return send_file(os.path.join(dname, "cards/0.png"))
+        except FileNotFoundError:
+            raise abortReason(404)
+    elif int(card_id) == 1:
+        try:
+            return send_file(os.path.join(dname, "cards/1.png"))
+        except FileNotFoundError:
+            raise abortReason(404)
     else:
-        abort(404)
+        raise abortReason(403)
 
-@app.errorhandler(404)
-def page_not_found(error):
-   return render_template('404.html', title = '404'), 404
+@app.route("/monomatch/icon/<filetype>/<icon_id>")
+def icon(filetype: str, icon_id: str):
+    if not icon_id.isdigit() or icon_id.find(".") != -1:
+        raise abortReason(403)
+
+    try:
+        if filetype.lower() == "png":
+            return send_file(f"generator/symbols/png/{int(icon_id)}.png")
+        elif filetype.lower() == "svg":
+            return send_file(f"generator/symbols/{int(icon_id)}.svg")
+        else:
+            raise abortReason(404, reason="Filetype not supported")
+    except FileNotFoundError as e:
+        raise abortReason(404, reason=f"Invalid image ID, valid range is 0-{len(os.listdir(os.path.join(dname, 'generator/symbols/png')))-1}")
+
+@app.errorhandler(abortReason)
+def page_not_found(error: abortReason):
+    if error.code == 403:
+        return render_template('error_message.html', errorCode='403', errorMessage="Forbidden", errorReason=error.reason), 403
+    elif error.code == 404:
+        return render_template('error_message.html', errorCode='404', errorMessage="Resource not found", errorReason=error.reason), 404
 
 def main():
     serve(app, host="0.0.0.0", port=8080)
@@ -46,4 +76,4 @@ if __name__ == "__main__":
         "FLASK_ENV": "development",
         "FLASK_DEBUG": "1"
     })
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080) #, use_reloader=False)
