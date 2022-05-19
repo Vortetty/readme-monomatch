0d718d6d5f91500a67f474f68f6f1cdd0fa5b5a6
winter <vortetty@gmail.com>
2022-05-19 15:51:01 -0700

    Better flask error handling and compression beginning

--- server/flaskImageProviderApp.py
+++ server/flaskImageProviderApp.py
@@ -9,8 +9,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+try:
+    match "":
+        case "":
+            pass
+except:
+    print("This program requires python >=3.10.0 due to the use of match/case")
+
 import json
 from flask import Flask, redirect, send_file, abort, render_template
+from flask_compress import Compress
 from waitress import serve
 import os
 from werkzeug.exceptions import HTTPException
@@ -21,6 +29,7 @@ dname = os.path.dirname(abspath)
 # os.chdir(dname)
 
 app = Flask(__name__)
+Compress(app)
 
 class abortReason (HTTPException):
     def __init__(self, code: int, reason: str=""):
@@ -70,10 +79,78 @@ def icon(filetype: str, icon_id: str|None=None):
 
 @app.errorhandler(abortReason)
 def page_not_found(error: abortReason):
-    if error.code == 403:
-        return render_template('error_message.html', errorCode='403', errorMessage="Forbidden", errorReason=error.reason), 403
-    elif error.code == 404:
-        return render_template('error_message.html', errorCode='404', errorMessage="Resource not found", errorReason=error.reason), 404
+    errorMessageText = ""
+    match error.code: # Match every standard error code defined by mozilla (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
+                      # 100, 101, 102, 103
+                      # 200, 201, 202, 203, 204, 205, 206, 207, 208, 226
+                      # 300, 301, 302, 303, 304, 305, 306, 307, 308
+                      # 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 425, 426, 426, 428, 429, 431, 451
+                      # 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511
+        case 100: errorMessageText = "Continue"
+        case 101: errorMessageText = "Switching Protocols"
+        case 102: errorMessageText = "Processing"
+        case 103: errorMessageText = "Early Hints"
+        case 200: errorMessageText = "OK"
+        case 201: errorMessageText = "Created"
+        case 202: errorMessageText = "Accepted"
+        case 203: errorMessageText = "Non-Authoritative Information"
+        case 204: errorMessageText = "No Content"
+        case 205: errorMessageText = "Reset Content"
+        case 206: errorMessageText = "Partial Content"
+        case 207: errorMessageText = "Multi-Status"
+        case 208: errorMessageText = "Already Reported"
+        case 226: errorMessageText = "IM Used"
+        case 300: errorMessageText = "Multiple Choices"
+        case 301: errorMessageText = "Moved Permanently"
+        case 302: errorMessageText = "Found"
+        case 303: errorMessageText = "See Other"
+        case 304: errorMessageText = "Not Modified"
+        case 305: errorMessageText = "Use Proxy"
+        case 306: errorMessageText = "Switch Proxy"
+        case 307: errorMessageText = "Temporary Redirect"
+        case 308: errorMessageText = "Permanent Redirect"
+        case 400: errorMessageText = "Bad Request"
+        case 401: errorMessageText = "Unauthorized"
+        case 402: errorMessageText = "Payment Required"
+        case 403: errorMessageText = "Forbidden"
+        case 404: errorMessageText = "Not Found"
+        case 405: errorMessageText = "Method Not Allowed"
+        case 406: errorMessageText = "Not Acceptable"
+        case 407: errorMessageText = "Proxy Authentication Required"
+        case 408: errorMessageText = "Request Timeout"
+        case 409: errorMessageText = "Conflict"
+        case 410: errorMessageText = "Gone"
+        case 411: errorMessageText = "Length Required"
+        case 412: errorMessageText = "Precondition Failed"
+        case 413: errorMessageText = "Payload Too Large"
+        case 414: errorMessageText = "URI Too Long"
+        case 415: errorMessageText = "Unsupported Media Type"
+        case 416: errorMessageText = "Range Not Satisfiable"
+        case 417: errorMessageText = "Expectation Failed"
+        case 418: errorMessageText = "I'm a teapot"
+        case 421: errorMessageText = "Misdirected Request"
+        case 422: errorMessageText = "Unprocessable Entity"
+        case 423: errorMessageText = "Locked"
+        case 424: errorMessageText = "Failed Dependency"
+        case 425: errorMessageText = "Too Early"
+        case 426: errorMessageText = "Upgrade Required"
+        case 428: errorMessageText = "Precondition Required"
+        case 429: errorMessageText = "Too Many Requests"
+        case 431: errorMessageText = "Request Header Fields Too Large"
+        case 451: errorMessageText = "Unavailable For Legal Reasons"
+        case 500: errorMessageText = "Internal Server Error"
+        case 501: errorMessageText = "Not Implemented"
+        case 502: errorMessageText = "Bad Gateway"
+        case 503: errorMessageText = "Service Unavailable"
+        case 504: errorMessageText = "Gateway Timeout"
+        case 505: errorMessageText = "HTTP Version Not Supported"
+        case 506: errorMessageText = "Variant Also Negotiates"
+        case 507: errorMessageText = "Insufficient Storage"
+        case 508: errorMessageText = "Loop Detected"
+        case 510: errorMessageText = "Not Extended"
+        case 511: errorMessageText = "Network Authentication Required"
+
+    return render_template('error_message.html', errorCode=error.code, errorMessage=errorMessageText, errorReason=error.reason), error.code
 
 def main():
     serve(app, host="0.0.0.0", port=8080)
